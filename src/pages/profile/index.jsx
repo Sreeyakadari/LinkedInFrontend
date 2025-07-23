1622776f5ec56e7b7b0f67922c1f6392e22bef56
SreeyaKadari <sreeyakadari2024@gmail.com>
2025-07-23 11:12:02 +0530

    updated code

---
+++ src/pages/profile/index.jsx
@@ -0,0 +1,143 @@
+import { getAboutUser } from "@/config/redux/action/authAction";
+import DashboardLayout from "@/layout/DashboardLayout";
+import UserLayout from "@/layout/UserLayout";
+import React, { useEffect, useState } from "react";
+import styles from "./index.module.css";
+import { BASE_URL, clientServer } from "@/config";
+import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { getAllPosts } from "@/config/redux/action/postAction";
+
+export default function ProfilePage() {
+  const authState = useSelector((state) => state.auth);
+  const postReducer = useSelector((state) => state.postReducer);
+  const [userProfile, setUserProfile] = useState({});
+  const [userPosts, setUserPosts] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAboutUser({ token: localStorage.getItem("token") }));
+    dispatch(getAllPosts());
+  }, []);
+
+  useEffect(() => {
+    if (authState.user != undefined) {
+      setUserProfile(authState.user);
+      let post = postReducer.posts.filter((post) => {
+        return post.userId.username === authState.user.userId.username;
+      });
+      setUserPosts(post);
+    }
+  }, [authState.user, postReducer.posts]);
+
+  const updateProfilePictue = async (file) => {
+    const formData = new FormData();
+    formData.append("profile_picture", file);
+    formData.append("token", localStorage.getItem("token"));
+
+    const response = await clientServer.post(
+      "/update_profile_picture",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+
+    dispatch(getAboutUser({ token: localStorage.getItem("token") }));
+  };
+
+  return (
+    <UserLayout>
+      <DashboardLayout>
+        {authState.user && userProfile.userId && (
+          <div className={styles.container}>
+            <div className={styles.backDropContainer}>
+              <label
+                htmlFor="profilePictureUpload"
+                className={styles.backDrop__overlay}
+              >
+                <p>Edit</p>
+              </label>
+              <input type="file" id="profilePictureUpload" />
+              <img
+                src={`${BASE_URL}/${userProfile.userId.profilePicture}`}
+                alt="backdrop"
+              />
+            </div>
+            <div className={styles.profileContainer__details}>
+              <div style={{ display: "flex", gap: "0.7rem" }}>
+                <div style={{ flex: "0.8" }}>
+                  <div
+                    style={{
+                      display: "flex",
+                      width: "fit-content",
+                      alignItems: "center",
+                      gap: "1.2rem",
+                    }}
+                  >
+                    <h2>{userProfile.userId.name}</h2>
+                    <p style={{ color: "grey" }}>
+                      @{userProfile.userId.username}
+                    </p>
+                  </div>
+
+                  <div>
+                    <p>{userProfile.bio}</p>
+                  </div>
+                </div>
+                <div style={{ flex: "0.2" }}>
+                  <h3>Recent Activity</h3>
+                  {userPosts.map((post) => {
+                    return (
+                      <div key={post._id} className={styles.postCard}>
+                        <div className={styles.card}>
+                          <div className={styles.card__profileContainer}>
+                            {post.media !== "" ? (
+                              <img
+                                src={`${BASE_URL}/${post.media}`}
+                                alt="post image"
+                              />
+                            ) : (
+                              <div
+                                style={{ width: "3.4rem", height: "3.4rem" }}
+                              ></div>
+                            )}
+                          </div>
+                          <p>{post.body}</p>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+            <div className={styles.workHistory}>
+              <h4>Work History</h4>
+              <div className={styles.workHistoryContainer}>
+                {userProfile.pastWork.map((work, index) => {
+                  return (
+                    <div key={index} className={styles.workHistoryCard}>
+                      <p
+                        style={{
+                          fontWeight: "bold",
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "0.8rem",
+                        }}
+                      >
+                        {work.company} - {work.position}
+                      </p>
+                      <p>{work.years}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        )}
+      </DashboardLayout>
+    </UserLayout>
+  );
+}
